537f0afeaac0940b6e3f0a61cec21a9adac784c0
MarkHomePro <chanon@nc.ac.th>
2025-07-17 11:38:26 +0700

    update card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
     return (
         <>
-            <div className='max-w-sm rounded-md overflow-hidden shadow-lg'>
-                <img className='w-full' src='https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg' alt='Placeholder' />
-                <div className='px-6 py-4 bg-white'>
-                    <div className='font-bold text-xl mb-2'>Card Title</div>
+            <div className='rounded-md overflow-hidden shadow-lg bg-white'>
+                <img className='w-full h-60 object-cover' src={props.imageUrl} alt={props.title} />
+                <div className='px-6 py-4'>
+                    <h2 className='font-bold text-xl mb-2'>{props.title}</h2>
                     <p className='text-gray-700 text-base'>
-                        This is a simple card component using Tailwind CSS for styling.
+                        {props.description}
                     </p>
                 </div>
-                <div className='px-6 py-4 flex items-center justify-between bg-white'>
+                <div className='px-6 py-4 flex items-center justify-between'>
                     <span className='text-gray-600 text-sm'>#tag</span>
-                    <a href="" className='text-blue-500 hover:text-blue-700'>Read More</a>
+                    <a href="" className='text-purple-500 hover:text-purple-700'>Read Mote</a>
                 </div>
             </div>
         </>
