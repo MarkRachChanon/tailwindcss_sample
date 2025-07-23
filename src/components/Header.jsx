9991d7835a7e10e227bd088156c5e93ae606d421
MarkHomePro <chanon@nc.ac.th>
2025-07-23 09:32:47 +0700

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <header className='bg-purple-950 text-white p-4'>
+            <div className='container mx-auto py-2'>
+                <div className='flex item-center justify-between'>
+                    <div className='flex items-center'>
+                        <img src="/vite.svg" alt="" className='inline-block mr-2 w-10'/>
+                        <h1 className='text-2xl font-bold'>Card Gallery</h1>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </>
+  )
+}
+
+export default Header
