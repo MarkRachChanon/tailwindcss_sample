27555f9c8c24fe3c061e262e48dec29f92d17058
MarkHomePro <chanon@nc.ac.th>
2025-07-23 09:46:27 +0700

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import CardList from './components/CardList'
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
+      <Header/>
       <div className='container mx-auto p-4'>
         <CardList />
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 
 const Header = () => {
-  return (
-    <>
-        <header className='bg-purple-950 text-white p-4'>
-            <div className='container mx-auto py-2'>
-                <div className='flex item-center justify-between'>
-                    <div className='flex items-center'>
-                        <img src="/vite.svg" alt="" className='inline-block mr-2 w-10'/>
-                        <h1 className='text-2xl font-bold'>Card Gallery</h1>
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const menuItems = [
+        {name: 'Home', link: '#home'},
+        {name: 'Collection', link: '#collec'},
+        {name: 'About', link: '#about'}
+    ]
+
+    return (
+        <>
+            <header className='bg-purple-950 text-white p-4'>
+                <div className='container mx-auto py-2'>
+                    <div className='flex item-center justify-between'>
+                        <div className='flex items-center'>
+                            <img src="/vite.svg" alt="" className='inline-block mr-2 w-10' />
+                            <h1 className='text-2xl font-bold'>Card Gallery</h1>
+                        </div>
+
                     </div>
                 </div>
-            </div>
-        </header>
-    </>
-  )
+            </header>
+        </>
+    )
 }
 
 export default Header
